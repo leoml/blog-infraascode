e6dfde59e7ba5bc6f3fe7750e833dfacde46e81e
Leonardo <leonardoml@gmail.com>
2020-01-19 07:22:12 -0300

    Adiciona script para realizar validação de arquivos do tipo YAML.

---
+++ posts/validacao-arquivos-yaml/yamllint.py
@@ -0,0 +1,19 @@
+#! /usr/bin/python3.6
+
+import yaml
+import sys
+
+if ( len(sys.argv) == 0 ) : 
+    print ("SORRY!! No file receivid.")
+    sys.exit(1)
+
+
+with open((sys.argv[1]), 'r') as stream:
+  try:
+    yaml.safe_load(stream)
+    print("[OK] - File (%s) validate with success! " % sys.argv[1])
+  except yaml.YAMLError as exc:
+    print("[Error] - File (%s) validated with error!" %  sys.argv[1])
+    print(exc)
+
+#EOF
